d37abefa8873fa9b14e39b2c6af9aa94910a362a
Wallace Barbosa <city1460911@gmail.com>
2021-03-07 00:12:46 -0300

    Erro bat file

--- bin/servidor.bat
+++ bin/servidor.bat
@@ -1,6 +1,6 @@
 @echo
 TITLE Server RMI
 cd %~dp0\bin
-java -Djava.security.policy=rmi.policy -Djava.rmi.server.useCodebaseOnly=false -Djava.rmi.server.hostname=localhost -Djava.rmi.server.codebase=file:%~dp0bin ServerRmi
+java  ServerRmi
 
 pause
