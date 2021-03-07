975b6b188d626f782ff054d82873c25a03d768a3
Wallace Barbosa <city1460911@gmail.com>
2021-03-07 00:23:02 -0300

    Update servidor.bat

--- bin/servidor.bat
+++ bin/servidor.bat
@@ -1,6 +1,6 @@
 @echo
 TITLE Server RMI
-cd %~dp0\bin
-java  ServerRmi
+
+java -Djava.security.policy=rmi.policy -Djava.rmi.server.useCodebaseOnly=false -Djava.rmi.server.hostname=localhost -Djava.rmi.server.codebase=file:%~dp0 ServerRmi
 
 pause
